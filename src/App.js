d8c8ff743e7cac17bc83fe73cb054f5c409ec1a9
pankaj kataria <pankajkt1808@gmail.com>
2023-07-25 09:52:20 +0530

    code optemise

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import './App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil, faPlus } from "@fortawesome/free-solid-svg-icons";
-
+import DeleteButton from "./icons/DeleteButton";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [list, setList] = useState([]);
-  const [completedTask, setCompletedTask] = useState([]);
 
-  function handleTodo(e) {
+  function handleTodoChange(e) {
     setTodo(e.target.value);
   }
 
   function handleAdd() {
     if (todo.trim() !== "") {
-      setList((prevList) => [...prevList, todo]);
-      setCompletedTask((prevTasks) => [...prevTasks, false]); // Initialize with false for new item
+      setList((prevList) => [...prevList, { text: todo, completed: false }]);
       setTodo("");
     }
   }
 
   function handleDelete(index) {
-    const updatedList = list.filter((item, i) => i !== index);
-    const updatedCompletedTasks = completedTask.filter((item, i) => i !== index);
-    setList(updatedList);
-    setCompletedTask(updatedCompletedTasks);
+    setList((prevList) => prevList.filter((item, i) => i !== index));
+  }
+
+  function handleToggleComplete(index) {
+    setList((prevList) =>
+      prevList.map((item, i) =>
+        i === index ? { ...item, completed: !item.completed } : item
+      )
+    );  
   }
 
   function handleKeyPress(event) {
@@ -33,66 +36,51 @@ function App() {
       handleAdd();
     }
   }
-
+  const completedTasksCount = useMemo(() => list.filter(item => item.completed).length, [list]);
   return (
     <div className="App">
       <div className='main'>
         <section id="todoList" >
           <header>
-            <h1>
+            <h1> 
               Todo App
             </h1>
-            <p>Completed: {completedTask.filter(item => item).length}/{list.length} </p>
+            <p>Completed: {completedTasksCount}/{list.length} </p>
           </header>
           <div className="row">
-            <button onClick={handleAdd}>
-              <FontAwesomeIcon icon={faPlus} className="add" />
-            </button>
             <div className='input'>
               <input
                 type="text"
                 placeholder="add a todo..."
                 name="input"
                 value={todo}
-                onChange={(e) => handleTodo(e)}
+                onChange={handleTodoChange}
                 onKeyDown={handleKeyPress}
                 className="inp-txt"
               />
             </div>
+            <button onClick={handleAdd}>
+              <FontAwesomeIcon icon={faPlus} className="add" />
+            </button>
           </div>
           <ul>
             {list.map((item, index) => (
-              <li className={`row align-center ${completedTask[index] ? "completed" : ""}`} key={index}>
+              <li
+                className={`row align-center ${item.completed ? "completed" : ""}`}
+                key={index}
+              >
                 <span className='row align-center'>
                   <input
                     id={`checkbox-${index}`}
                     type="checkbox"
-                    checked={completedTask[index]}
-                    onChange={() => {
-                      const updatedTasks = [...completedTask];
-                      updatedTasks[index] = !completedTask[index];
-                      setCompletedTask(updatedTasks);
-                    }}
+                    checked={item.completed}
+                    onChange={() => handleToggleComplete(index)}
                   />
-                  <span className="item">{item}</span>
+                  <span className="item">{item.text}</span>
                 </span>
-                {/* <FontAwesomeIcon icon={faPencil} /> */}
+                {/* <FontAwesomeIcon icon={faPencil} className="update" /> */}
                 <button className="danger" onClick={() => handleDelete(index)}>
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
-                    <line x1="18" y1="9" x2="12" y2="15"></line>
-                    <line x1="12" y1="9" x2="18" y2="15"></line>
-                  </svg>
+                 <DeleteButton/>
                 </button>
               </li>
             ))}
